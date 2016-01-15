569484e5ac1e23280c7c60d00653529442271332
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-01-15 06:31:14 +0100

    formatting

--- src/main/scala/pyspark_elastic/PythonHelper.scala
+++ src/main/scala/pyspark_elastic/PythonHelper.scala
@@ -1,25 +1,25 @@
 package pyspark_elastic
 
 import java.util.{ Map => JMap }
-
 import scala.collection.JavaConversions.mapAsScalaMap
-
 import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.rdd.RDD
 import org.elasticsearch.spark.sparkContextFunctions
 import org.elasticsearch.spark.sparkStringJsonRDDFunctions
-
 import pyspark_util.Pickling
 import pyspark_util.Pickling.toPickleableRDD
 import pyspark_util.Pickling.toUnpickleableRDD
+import org.elasticsearch.spark.rdd.ScalaEsRDD
+import org.elasticsearch.spark.rdd.ScalaEsRDD
 
 class PythonHelper() {
 
   implicit val pickling = new Pickling()
 
   def esJsonRDD(sc: JavaSparkContext, cfg: JMap[String, String]) = {
-    JavaRDD.fromRDD(sc.sc.esJsonRDD(config(cfg)))
+    val rdd = sc.sc.esJsonRDD(config(cfg))
+    JavaRDD.fromRDD(rdd)
   }
 
   def saveJsonToEs(rdd: JavaRDD[Array[Byte]], cfg: JMap[String, String]) = {
