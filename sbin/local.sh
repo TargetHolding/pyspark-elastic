3b869deeacd1455109e6789642ecb30adb2b419c
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-03-22 10:55:36 +0100

    use version from version.txt and use right build artifact

--- sbin/local.sh
+++ sbin/local.sh
@@ -1,6 +1,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
-VERSION=`grep version $DIR/build.sbt | sed 's/version := //g' | sed 's/"//g'`
+VERSION=`cat version.txt`
 
 PYSPARK_DRIVER_PYTHON=ipython \
 	$DIR/lib/spark-1.6.1-bin-hadoop2.6/bin/pyspark \
@@ -8,5 +8,5 @@ PYSPARK_DRIVER_PYTHON=ipython \
 	--driver-memory 2g \
 	--master local[*] \
 	--jars $DIR/target/scala-2.10/pyspark-elastic-assembly-$VERSION.jar \
-	--py-files $DIR/target/pyspark_elastic-$VERSION-py2.7.egg \
+	--py-files target/scala-2.10/pyspark-elastic-assembly-$VERSION.jar \
 	$@
